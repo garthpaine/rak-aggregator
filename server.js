5b8a24bf32b062505a4f9218e1c18e480b8d0376
Ubuntu <ubuntu@ip-172-30-0-144.us-west-2.compute.internal>
2020-12-15 23:03:32 +0000

    ac

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8080;
+var port = 1234;
+const https = require("https");
 
 app.get("/", function (req, res) {
     res.redirect("index.html")
 });
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
 app.use(methodOverride());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
@@ -17,3 +24,34 @@ app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
+
+
+setInterval(function(){
+	const options = {
+	    "method": "GET",
+	    "hostname": "raksensors.data.thethingsnetwork.org",
+	    "port": 443,
+	    "path": "/api/v2/query?last=5s",
+	    "headers": {
+	      "Accept": "application/json",
+	      "Authorization":"key ttn-account-v2.jNNxl6FFP5WrkPqxEpY-HH70L1BcW8LAsXGGFhYA3yQ"
+	    }
+	}
+
+	const req = https.request(options, function(res) {
+
+	    var chunks = [];
+
+	    res.on("data", function (chunk) {
+		chunks.push(chunk);
+	    });
+
+	    res.on("end", function() {
+		var body = Buffer.concat(chunks);
+		console.log(body.toString());
+	    });
+
+	});
+
+	req.end()
+},4500);
